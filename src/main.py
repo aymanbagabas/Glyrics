85ad2d588c97e5134e4f3e1c81db42bd97c86ff9
Ayman Bagabas <ayman.bagabas@gmail.com>
2019-01-09 01:37:26 -0500

    Update to version 0.1.2
    This include lots of bugfixes and code improvements and some cleaning.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # main.py
 #
-# Copyright 2018 Ayman Bagabas
+# Copyright 2019 Ayman Bagabas
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -16,172 +16,210 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import os
 import gi
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Playerctl', '2.0')
 
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, GLib
 
 from .window import GlyricsWindow
 
 from gi.repository import Playerctl
 
-from glyrics import version as VERSION
-
 from plyr import Query, Database
 
-from time import sleep
 from threading import Thread
 
-db = Database("/tmp")
+db_dir = os.path.join(os.environ["HOME"], ".local/share/glyrics")
+if not os.path.exists(db_dir):
+    os.makedirs(db_dir)
+db = Database(db_dir)
+queries = []
 
 class GlyricsQuery(Query):
     def __init__(self, **kwargs):
         self.database = db
         self.db_autoread = True
-        self.useragent = "glyrics/" + VERSION + "(Baby Vanilla) +https://www.github.com/aymanbagabas/glyrics"
+        self.db_autowrite = True
         self.force_utf8 = True
+        self.parallel = 20
+        self.providers = 'all'
+        # TODO: respect system proxy settings
+        self.useragent = "glyrics/" + VERSION + "+https://www.github.com/aymanbagabas/glyrics"
+
+        queries.append(self)
+
 
 class Application(Gtk.Application):
-    def __init__(self, version):
-        super().__init__(application_id='org.gnome.Glyrics',
-                         flags=Gio.ApplicationFlags.FLAGS_NONE)
-        
+    def __init__(self):
+        super().__init__(application_id='com.aymanbagabas.Glyrics',
+                         flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE)
+
         self.playermanager = Playerctl.PlayerManager()
         self.player = None
-        self.metadata = None
-        self.queries = []
-        
-        
-    def update_player(self, player):
+
+        self.add_main_option("version", ord("v"), GLib.OptionFlags.NONE, GLib.OptionArg.NONE, "Show Glyrics version", None)
+
+
+    def set_player(self, player):
         win = self.props.active_window
-        
+
         # Disconnect signal from previous player
         if self.player:
-            self.player.disconnect(self.id)
-            
-        self.player = player
-        self.id = player.connect("metadata", self.on_metadata, self.playermanager)
-        self.metadata_change(player)
+            self.player.disconnect(self.player_id)
+        self.player_id = player.connect("metadata", self.on_metadata)
+
         self.playermanager.move_player_to_top(player)
-        
-        
-    def set_player(self, player_name):
-        player = Playerctl.Player.new(player_name)
-        
-        if player != self.player:
-            self.update_player(player)
-            
-            
-    def query_callback(self, cache, query):
+        self.player = player
+        self.on_metadata(player, player.props.metadata)
+
+
+
+    def query_commit(self, query, player):
         win = self.props.active_window
-        if query.get_type == "lyrics":
-            win.update_lyrics(cache.data.decode())
-            
-        return 'pre_stop'
-    
-        
-    def metadata_query(self, **args):
-        qry = GlyricsQuery(**args)
-        self.queries.append(qry)
-        Thread(target=qry.commit).start()
-        
-        
-    def metadata_change(self, player):
-        print("on metadata, {}".format(player.props.player_name))
+        items = query.commit()
+
+        win.stop_spinner()
+
+        if len(items) > 0 and self.player == player \
+            and self.player.get_title() == query.title:
+                win.set_lyrics(items[0].data.decode())
+                # FIXME: Some players like Spotify emits a metadata change signal
+                # on events like 'add to queue' and others which cause multiple
+                # calls to get contents. Currently, cancelling all other queries
+                # is used which is very dirty.
+                self.cancel_all_queries()
+        elif len(items) == 0 and self.player == player \
+            and self.player.get_title() == query.title \
+            and len(db.lookup(query)) == 0:
+                # No data found
+                win.clear_window("No lyrics found")
+        else:
+                win.clear_window("No lyrics found")
+
+        queries.remove(query)
+
+
+    def on_metadata(self, player, metadata):
         win = self.props.active_window
-        
-        win.pre_metadata()
-        
+        query = GlyricsQuery(get_type="lyrics")
         subtitle = None
         title = player.get_title()
         artist = player.get_artist()
         album = player.get_album()
-        
+
+        win.pre_metadata()
+
         if title:
+            query.title = title
+            query.normalize = ("title")
             if artist and album:
                 subtitle = "by {} on {}".format(artist, album)
+                query.normalize += ("artist", "album")
+                query.artist = artist
+                query.album = album
             elif artist and not album:
                 subtitle = "by {}".format(artist)
+                query.normalize += ("artist")
+                query.artist = artist
             elif not artist and album:
                 subtitle = "on {}".format(artist, album)
-                
-            self.metadata_query(title=title, artist=artist, album=album, get_type="lyrics", callback=self.query_callback)
+                query.normalize += ("album")
+                query.album = album
+
+            t = Thread(target=self.query_commit, args=(query, self.player,))
+            t.daemon = True
+            t.start()
+        else:
+            win.clear_window("No song info provided")
 
         win.set_title(title=title, subtitle=subtitle)
-        
-    
-    def on_metadata(self, player, metadata, manager):
-        if self.metadata == metadata:
-            return
-        self.metadata = metadata
-            
-        self.metadata_change(player)
-        
+
     def init_player(self, name):
         win = self.props.active_window
-        
         player = Playerctl.Player.new_from_name(name)
-        
-        self.update_player(player)
+
         self.playermanager.manage_player(player)
-        
-    
+
+
     def on_name_appeared(self, manager, name):
         player = Playerctl.Player.new_from_name(name)
         self.playermanager.manage_player(player)
-        
-        
+
+
     def on_player_appeared(self, manager, player):
         win = self.props.active_window
-        
+
         if len(manager.props.players) == 1:
-            self.update_player(player)
-        
-        win.update_players_menu(self)
-        
-    
+            self.set_player(player)
+
+        win.update_players_menu()
+
+
     def on_player_vanished(self, manager, player):
         win = self.props.active_window
-        
-        if len(manager.props.players) >= 1:
-            self.update_player(manager.props.players[-1])
-        
-        win.update_players_menu(self)
-        
-        
+
+        if len(manager.props.players) >= 1 \
+            and self.player == player:
+            self.set_player(manager.props.players[0])
+
+        if len(self.playermanager.props.players) == 0:
+            win.clear_window("No player available")
+
+        win.update_players_menu()
+
+
     def do_startup(self):
         Gtk.Application.do_startup(self)
-        
+
         action = Gio.SimpleAction.new("quit", None)
         action.connect("activate", self.on_quit)
         self.add_action(action)
-        
+
 
     def do_activate(self):
         win = self.props.active_window
         if not win:
             win = GlyricsWindow(application=self)
+
         win.present()
-        
         self.playermanager.connect("name-appeared", self.on_name_appeared)
         self.playermanager.connect("player-appeared", self.on_player_appeared)
         self.playermanager.connect("player-vanished", self.on_player_vanished)
-        
+
         for name in self.playermanager.props.player_names:
             self.init_player(name)
-            
-        win.update_players_menu(self)
-        
-        
-    def on_quit(self, action, param):
-        for q in self.queries:
+
+        if len(self.playermanager.props.players) == 0:
+            win.clear_window("No player available")
+
+
+    def cancel_all_queries(self):
+        for q in queries:
             Thread(target=q.cancel).start()
-        
+
+
+    def on_quit(self, action, param):
+        self.cancel_all_queries()
         self.quit()
 
 
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        options = options.end().unpack()
+
+        if "version" in options:
+            print("Glyrics {}".format(VERSION))
+            return 0
+
+        self.activate()
+        return 0
+
+
 def main(version):
-    app = Application(version)
+    global VERSION
+    VERSION = version
+    app = Application()
     return app.run(sys.argv)
--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 # window.py
 #
-# Copyright 2018 Ayman Bagabas
+# Copyright 2019 Ayman Bagabas
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -31,9 +31,9 @@ class GlyricsWindow(Gtk.ApplicationWindow):
     lyrics_window = GtkTemplate.Child()
     lyrics = GtkTemplate.Child()
     spinner = GtkTemplate.Child()
-    
+
     spinner_timeout_id = None
-    
+ 
     group = Gtk.RadioButton.new(None)
 
 
@@ -41,41 +41,42 @@ class GlyricsWindow(Gtk.ApplicationWindow):
         super().__init__(**kwargs)
         self.init_template()
         self.players_box.show_all()
-        
-        
-    def update_lyrics(self, lyrics):
-        if self.spinner_timeout_id:
-            GLib.source_remove(self.spinner_timeout_id)
-            self.spinner_timeout_id = None
+
+
+    def set_lyrics(self, lyrics):
         self.lyrics.set_text(lyrics)
+
+        if self.spinner_timeout_id:
+            self.remove_spinner()
         if self.spinner in self:
             self.remove(self.spinner)
         elif self.label in self:
             self.remove(self.label)
-        
-        self.add(self.lyrics_window)
-        
-        
-    def on_spinner_timeout(self, *args, **kwargs):
-        if self.spinner_timeout_id:
-            GLib.source_remove(self.spinner_timeout_id)
-            self.spinner_timeout_id = None
-            self.clear_window("No lyrics found!")
-            return True
         else:
-            return False
-        
-        
+            return
+
+        self.add(self.lyrics_window)
+        pos = self.lyrics_window.get_vadjustment()
+        pos.set_value(0)
+        self.lyrics_window.set_vadjustment(pos)
+
+
+    def stop_spinner(self):
+        self.spinner.stop()
+
+
     def pre_metadata(self):
         if self.label in self:
             self.remove(self.label)
         elif self.lyrics_window in self:
             self.remove(self.lyrics_window)
+        else:
+            return
+
         self.add(self.spinner)
         self.spinner.start()
-        self.spinner_timeout_id = GLib.timeout_add(10000, self.on_spinner_timeout, None)
-        
-        
+
+
     def set_title(self, title=None, subtitle=None):
         if title:
             self.header_bar.props.title = title
@@ -85,8 +86,8 @@ class GlyricsWindow(Gtk.ApplicationWindow):
             self.header_bar.props.subtitle = subtitle
         else:
             self.header_bar.props.subtitle = None
-        
-        
+
+
     def clear_window(self, label=None):
         if self.lyrics_window in self:
             self.remove(self.lyrics_window)
@@ -100,36 +101,37 @@ class GlyricsWindow(Gtk.ApplicationWindow):
             if self.spinner_timeout_id:
                 GLib.source_remove(self.spinner_timeout_id)
                 self.spinner_timeout_id = None
-            self.label.props.label = "No player available!"
-        
-        
-    def on_radio_toggled(self, radio, app):
+            self.label.props.label = ""
+
+
+    def on_radio_toggled(self, radio, player):
+        app = self.props.application
+
         if radio.get_active():
-            app.set_player(radio.props.label)
-        
-        
-    def update_players_menu(self, app):
+            app.set_player(player)
+
+
+    def update_players_menu(self):
+        app = self.props.application
+
+        # In case of no player available
         if len(app.playermanager.props.players) == 0:
-            self.clear_window()
             self.players_button.set_sensitive(False)
             self.set_title()
             return
         else:
-            if self.label in self:
-                self.remove(self.label)
-                self.add(self.lyrics_window)
             self.players_button.set_sensitive(True)
-        
+
         for child in self.players_box:
             self.players_box.remove(child)
-        
+
         for player in app.playermanager.props.players:
             radio = Gtk.RadioButton.new_with_label_from_widget(self.group, player.props.player_name)
-            
+
             if player == app.player:
                 radio.set_active(True)
-            
+
             self.players_box.add(radio)
-            radio.connect("toggled", self.on_radio_toggled, app)
-        
+            radio.connect("toggled", self.on_radio_toggled, player)
+
         self.players_box.show_all()
