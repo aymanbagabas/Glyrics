2fe13c8a060c1160806d379cd1c3d184afb95e4f
Ayman Bagabas <ayman.bagabas@gmail.com>
2019-01-12 15:52:41 -0500

    Change LICENSE to GPLv3

--- src/gi_composites.py
+++ src/gi_composites.py
@@ -2,7 +2,7 @@
 # Copyright 2015 Dustin Spicuzza <dustin@virtualroadside.com>
 #
 # This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
+# modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 #
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
+# You should have received a copy of the GNU General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
 # USA
